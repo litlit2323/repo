e1b2aa46e3dc38a6e962ef988bc61f6511e26f51
100pecheneK <mistermihail23@gmail.com>
2020-11-06 11:42:19 +0500

    as

--- next.config.js
+++ next.config.js
@@ -2,6 +2,27 @@ require('dotenv').config()
 
 module.exports = {
   env: {
-    API_URL: process.env.API_URL
+    API_URL: process.env.API_URL,
+  },
+  async headers() {
+    return [
+      {
+        // mathching all API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ]
   },
 }
